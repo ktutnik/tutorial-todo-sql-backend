347579916092c718360d655add24fcbb25c66bc0
Ketut Sandiarsa <ktutnik@gmail.com>
2019-06-05 05:53:53 +0800

    Export baseTable function inside migration

--- db/migrations/v1.0.0.ts
+++ db/migrations/v1.0.0.ts
@@ -1,6 +1,6 @@
 import * as Knex from "knex";
 
-function baseTable(t: Knex.CreateTableBuilder, knex: Knex) {
+export function baseTable(t: Knex.CreateTableBuilder, knex: Knex) {
     t.bigIncrements("id")
     t.timestamp("createdAt").defaultTo(knex.fn.now())
     t.boolean("deleted").defaultTo(false)
